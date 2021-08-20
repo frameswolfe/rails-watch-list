50571fc7383b9db347f2874e5e95a991df8d1e44
James Wolfe <jr.wolfe@outlook.com>
2021-08-20 15:41:19 +0100

    completed controllers and views, updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+if Rails.env.development?
+  puts "Destroying movie lists..."
+  List.destroy_all
+  puts "Destroying movies..."
+  Movie.destroy_all
+end
+
+puts "Creating movies..."
+
+10.times do |i|
+  puts "Importing movies from page #{i + 1}"
+  movies_url = 'http://tmdb.lewagon.com/movie/top_rated'
+  base_poster_url = 'https:image.tmdb.org/t/p/original'
+  movies = JSON.parse(open("#{movies_url}?page=#{i + 1}").read)['results']
+  movies.each do |movie|
+    puts "Creating #{movie['title']}..."
+    Movie.create(
+      title: movie['title'],
+      overview: movie['overview'],
+      poster_url: "#{base_poster_url}#{movie['poster_path']}",
+      rating: movie['vote_average']
+    )
+  end
+end
+
+puts "Creating lists..."
+
+10.times do |i|
+  list = List.create!(name: "#{i+1}. #{Faker::Movies::LordOfTheRings.location}")
+  puts "Creating #{list['name']}..."
+end
+
+puts "Creating bookmarks..."
+
+10.times do
+  bookmark = Bookmark.create!(
+    comment: "recommended by #{Faker::Movies::LordOfTheRings.character}",
+    movie_id: rand(Movie.first.id..Movie.last.id),
+    list_id: rand(List.first.id..List.last.id)
+  )
+  puts "Creating bookmark #{bookmark['comment']}..."
+end
+
+puts "Finished seeding!"
